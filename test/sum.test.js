e80e5baee4dc2940e9b626de91c62c60c082840b
falgunshah7 <52358512+falgunshah7@users.noreply.github.com>
2025-07-14 18:43:08 +0530

    feat: add multi function

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract } = require("../index.js");
+const { sum, subtract, multiply } = require("../index.js");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("subtracts 5 - 2 to equal 3", () => {
   expect(subtract(5, 2)).toBe(3);
+});
+
+test("multiplies 2 * 2 to equal 4", () => {
+  expect(multiply(2, 2)).toBe(4);
 });
